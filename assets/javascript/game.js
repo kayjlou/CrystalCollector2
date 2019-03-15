d0474de5a80de4e269f59e22d320a0d78269325d
Katie Lewis <klewi314@gmail.com>
2019-03-14 19:33:43 -0700

    Updated JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,69 +7,97 @@ let images = [
 ];
 
 //Initial values for wins/losses/score
-let wins = 0;
-let score = 0;
-let losses = 0;
+var wins = 0;
+var score = 0;
+var losses = 0;
+let goal = Math.floor(Math.random() * 100) + 40;
 
 //Function to get random value
-getRand = () = {
-  math
-}
+getRand = (max, min) => {
+  return Math.floor(Math.random() * max) + min;
+};
 
 //Assign values to the buttons and goals
-let btn0 =
-let btn1 =
-let btn2 =
-let btn3 = 
+// getNum = () => {
+//   // let goal = getRand(80, 20);
+//   console.log("The goal is " + goal);
+//   document.querySelector("#goal").innerHTML = goal;
+// };
 
-
-//Function that creates the buttons dynamically on page 
+//Function that creates the buttons dynamically on page
 createBtns = () => {
   for (let i = 0; i < 4; i++) {
     let image = document.createElement("img");
     image.className = "crystals";
     image.setAttribute("id", "btn" + i);
-    image.setAttribute("value", i + 1);
+    image.setAttribute("value", getRand(20, 1));
     image.setAttribute("src", images[i]);
     document.querySelector(".images").append(image);
+    document.querySelector("#goal").innerHTML = goal;
   }
 };
 
-//Run create buttons 
-createBtns();
+init = () => {
+  // getNum();
+  createBtns();
+  // check();
+};
+//Initialize game
+init();
 
 //Add event listener to page to check if crystal is clicked
 document.addEventListener("click", e => {
+  //Checks id of button
   console.log(e.target.id);
+  // name = e.target.id;
+
   switch (e.target.id) {
-    case btn0:
-      console.log("0");
+    case "btn0":
       addScore(btn0);
+      check();
       break;
 
-    case btn1:
-      console.log("1");
+    case "btn1":
       addScore(btn1);
+      check();
       break;
 
-    case btn2:
-      console.log("1");
+    case "btn2":
       addScore(btn2);
+      check();
       break;
 
-    case btn3:
-      console.log("1");
+    case "btn3":
       addScore(btn3);
+      check();
       break;
-
-
   }
 });
 
-//Function to add crystal value to score 
-addScore = value => {
-  score = score + value;
-  console.log(value)
+//Function to add crystal value to score
+addScore = buttonName => {
+  //Grab value of button that is clicked
+  score += parseInt(buttonName.getAttribute("value"));
+  // console.log(`The value is ${buttonName.getAttribute("value")}`);
+  // console.log(score);
   //display in html
-  document.querySelector("#score").innerHtml = score;
+  document.querySelector("#score").innerHTML = score;
+};
+
+check = () => {
+  if (score === goal) {
+    // console.log("YOU WON!!!");
+    wins++;
+    document.querySelector("#update").innerHTML = "OMG YOU WON!!!";
+    document.querySelector("#wins").innerHTML = wins;
+  } else if (score > goal) {
+    // console.log("YOU LOSE");
+    losses++;
+    document.querySelector("#losses").innerHTML = losses;
+    document.querySelector("#update").innerHTML = "YOU LOST!!!";
+    // reset()
+  } else {
+    // console.log("Keep going");
+    document.querySelector("#update").innerHTML = "Keep clicking";
+  }
 };
---
+++ assets/javascript/psuedo.js
@@ -0,0 +1,29 @@
+//2 options: have buttons static on page in HTML with on click events or dynamically create buttons
+
+//define variables
+//wins
+//losses
+//goalNum= randomnumberfunction()
+
+//Generate random numbers (for goalNum & crystals)
+//USE Math.floor(Math.random()) -- don't forget max/min
+
+//after numbers are randomly generated update HTML to show goalNum
+
+//Function to update total (this function will be called when buttons are clicked on page)
+//Will look like  total = total + (crystal random number)
+//Update HTML to show crystal
+
+//Function to check if win or lose
+//Check if total===goalNum (if so increment wins, if not increment losses)
+//Update HTML to either wins or losses
+
+//Click events for each button
+//This function will run the updateTotal function based on which button is clicked
+
+//Reset Function
+//Reset variables (goalNum) (Could keep wins and losses if you want them incremented up to a certain number)
+//Get random numbers again
+
+//These are just function declarations I pseudo-coded -- Make sure to call the functions in the order you want to use them
+//Results are endless :) Hope this helps!
